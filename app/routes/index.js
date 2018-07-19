fc71beb4cf0a78542ebdd8aa8b5f93d6f0532c62
magnum <k.o.m.888@i.ua>
2018-07-19 20:16:33 +0300

    add routes

--- app/routes/index.js
+++ app/routes/index.js
@@ -16,7 +16,8 @@ module.exports = function routes(app, passport) {
     // Controllers
         indexController  = require('../controllers/indexController'),
         loginController  = require('../controllers/loginController'),
-        secureController = require('../controllers/secureController');
+        secureController = require('../controllers/secureController'),
+        goodsController = require('../controllers/goodsController');
 
     var router = new Router();
 
@@ -24,7 +25,9 @@ module.exports = function routes(app, passport) {
         .get('/',          indexController.index)
         .get('/users',     indexController.list)
         .get('/users/:id', indexController.getId)
-
+        .get('/goods/:is', goodsController.getGoodsById) /*200 - ok, 404 - !ok*/
+        .post('/goods',    goodsController.postGoods) /*201 - added, 400 - !added*/
+        .del('/goods/:id', goodsController.delGoodsById) /*204 - deleted, 400 - !deleted*/
         .get('/login',     loginController.login)
         .post('/login',
             passport.authenticate('local', {
