573feafc45d4775dc150fbab35268e8fe8b6bec3
magnum <k.o.m.888@i.ua>
2018-07-20 03:36:46 +0300

    changed goodsController

--- app/controllers/goodsController.js
+++ app/controllers/goodsController.js
@@ -8,36 +8,35 @@ const memcachedManager = require('../managers/memcachedManager'),
 
 async function getGoods(ctx, next){
     try {
-    ctx.body = await memcachedManager.getById(ctx.params.id);
-    if(ctx.body) ctx.status = 200;
-    else ctx.status = 404;
+        ctx.body = await memcachedManager.getById(ctx.params.id);
+        if(ctx.body) ctx.status = 200;
+        else ctx.status = 404;
     } catch (e) {
-    ctx.status = 404;
+        ctx.status = 404;
     }
     await next();
 }
 
 async function postGoods(ctx, next){
     try {
-    let key = keyUnique();
-    await memcachedManager.setById(key, ctx.request.body);
-    ctx.status = 201;
-    ctx.body = key;
+        let key = keyUnique();
+        await memcachedManager.setById(key, ctx.request.body);
+        ctx.status = 201;
+        ctx.body = key;
     } catch (e) {
-    ctx.status = 400;
+        ctx.status = 400;
     }
     await next();
 }
 
 async function delGoods(ctx, next){
     try {
-    await memcachedManager.delById(ctx.params.id);
-    ctx.status = 204;
-    await next();
+        await memcachedManager.delById(ctx.params.id);
+        ctx.status = 204;
     } catch (e) {
-    ctx.status = 400;
-    await next();
+        ctx.status = 400;
     }
+    await next();
 }
 
 module.exports = {getGoods, postGoods, delGoods};
